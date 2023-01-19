0643678bdbe8964195b0111c6acecf77cd58b4ff
Julyanne Correia <julyanne.es@gmail.com>
2023-01-18 23:35:44 -0300

    new page and component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Home from "./pages/Home";
+import Favorite from "./pages/Favorite";
+import Header from "./components/Header";
 
 import 'react-toastify/dist/ReactToastify.min.css';
 
@@ -8,8 +10,10 @@ function App() {
   return (
     <BrowserRouter>
       <ToastContainer autoClose={3000}/>
+      <Header/>
       <Routes>
         <Route path="/" element={ <Home/> }/>
+        <Route path="/favorite" element={ <Favorite/> }/>
       </Routes>
     </BrowserRouter>
   );
