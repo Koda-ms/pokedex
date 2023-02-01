275073c198d349895e7e2013b613c34d50decd49
Julyanne Correia <julyanne.es@gmail.com>
2023-01-31 21:15:43 -0300

    fix&feat: fixing the favorite icon; adding filter

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,37 +1,50 @@
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
-import { FiPlusCircle, FiSearch } from 'react-icons/fi';
-import './home.css';
-import api, { getPokemonsData, listPokemons } from '../../services/api';
+import { FiArrowRightCircle, FiArrowLeftCircle, FiSearch } from 'react-icons/fi';
+import api, { getPokemonsData, listPokemons, getPokemonsTypes } from '../../services/api';
+//import useTheme from "./use-theme";
 import Modal from '../../components/Modal';
+import './home.css';
 
 function Home(){
     const[loading, setLoading] = useState(true);
     const[pokemonName, setPokemonName] = useState('');
     const[pokemonData, setPokemonData] = useState([]);
     const[pokemonDetail, setPokemonDetail] = useState([]);
+    const[pokemonsTypes, setPokemonsTypes] = useState([]);
     const[postModal, setPostModal] = useState(false);
+    const[page, setPage] = useState(0);
+    const[totalPages, setTotalPages] = useState(0);
+
+    const itensPerPage = 21;
+
+    //useTheme(themes[themeIndex]);
 
     useEffect(() => {
         async function loadPokemons(){
-
+      
            try {
-            const data = await listPokemons();
-            const promises = data.map(async (pokemon) =>{
+            const data = await listPokemons(itensPerPage, itensPerPage*page);
+            //console.log(data.results);
+            const promises = data.results.map(async (pokemon) =>{
                 return await getPokemonsData(pokemon.url);
             })
-
+            
             const results = await Promise.all(promises);
-            console.log(results)
             setPokemonData(results);
 
+            const pokeTypes = await getPokemonsTypes();
+            const typesResults = await Promise.all(pokeTypes.map(type => type.name));
+            setPokemonsTypes(typesResults);
+
             setLoading(false);
+            setTotalPages(Math.ceil (data.count / itensPerPage));
            } catch (error) {
             console.log('loading pokemons error: ', error);
            }
         }
         loadPokemons();
-    }, []);
+    }, [page]);
 
     async function showPokemon(){
         const toArray = [];
@@ -40,6 +53,10 @@ function Home(){
         .then((response) => {
             toArray.push(response.data);
             setPokemonData(toArray);
+            const elem1 = document.getElementById('pages');
+            const elem2 = document.getElementById('right-btn');
+            elem1.style.display = 'none';
+            elem2.style.display = 'none';
         })
         .catch((error) => {
             toast.error('This pokemon does not exist. Please, try again.');
@@ -57,24 +74,44 @@ function Home(){
         setPokemonDetail(pokemon);
     }
 
+    function handlePage(action){
+        setPage(action === 'next' ? page + 1 : page - 1);
+    }
+
+    // WORKING ON IT LATER
+    function handleTypeFilter(type){
+        alert("test")
+    }
+    
     return(
         <div className='container'>
-            <form onSubmit={handleSearch}>
-                <label className='search-area'>
-                    <input type='text' placeholder='Search pokemon'
-                    onChange={(e) => setPokemonName(e.target.value.toLowerCase())}/>
-                    <button type='submit'>
-                        <FiSearch className='search-icon' size={18} color='#000'/>
-                    </button>
-                </label>
-            </form>
+            <div className='search-filter'>
+                <form onSubmit={handleSearch}>
+                    <label className='search-area'>
+                        <input type='text' placeholder='Search pokemon'
+                        onChange={(e) => setPokemonName(e.target.value.toLowerCase())}/>
+                        <button type='submit'>
+                            <FiSearch className='search-icon' size={18} color='#000'/>
+                        </button>
+                    </label>
+                </form>
+
+                <select className='slc-type'>
+                    <option>Filter type</option>
+                    {pokemonsTypes.map((type) => {
+                        return(
+                            <option onClick={handleTypeFilter(type)}>{type}</option>
+                        )
+                    })}
+                </select>
+            </div>
 
             {loading ? (<div>Loading info, hold on...</div>)
             : (
                 <div className='content-area'>
                 {pokemonData.map((pokemon) => {
                         return(
-                            <div className='content' key={pokemon.id}>
+                            <div className={`content ${pokemon.types[0].type.name}`} key={pokemon.id}>
                                 <div>
                                     <span>#{pokemon.id}</span>
                                     <h4 onClick={() => toggleModal(pokemon)}>{pokemon.name}</h4>
@@ -83,11 +120,11 @@ function Home(){
                                 <table className='tb-pokemons'>
                                     <tbody>
                                         <tr>
-                                            <td>{pokemon.types[0].type.name}</td>
-                                            {pokemon.types.length > 1 ? 
-                                            (<td>{pokemon.types[1].type.name}</td>)
-                                            : <></>
-                                            }
+                                            {pokemon.types.map((type) => {
+                                                return (
+                                                    <td key={type.type.name}>{type.type.name}</td>
+                                                )
+                                            })}
                                         </tr>
                                     </tbody>
                                 </table>
@@ -96,6 +133,18 @@ function Home(){
                     })}
                 </div>
             )}
+
+            <label id='pages' className='page-num'>{page+1} of {totalPages}</label>
+            
+            <button id= 'right-btn' className='pagination-right' onClick={() => handlePage('next')}>
+                <FiArrowRightCircle size={30} color='#121212'/>
+            </button>
+
+            {page !== 0 ? 
+            (<button className='pagination-left' onClick={() => handlePage('back')}>
+                <FiArrowLeftCircle size={30} color='#121212'/>
+            </button>) : <></>}
+            
             {postModal && (
                 <Modal pokemon={pokemonDetail}
                 close={toggleModal}/>
